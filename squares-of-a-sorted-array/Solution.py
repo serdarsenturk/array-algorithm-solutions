1bbbb4c23fb55a6bfc492e8b83e0556fb14a5417
Serdar <serdarsenturk@windowslive.con>
2019-06-20 23:35:42 +0300

    Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.

---
+++ squares-of-a-sorted-array/Solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def sortedSquares(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        
+        for index in range(len(A)):
+            A[index] = A[index]**2
+        
+        for i in range(len(A)):
+            for j in range(len(A)-1):
+                if A[j] > A[j+1]:
+                    temp = A[j+1]
+                    A[j+1] = A[j]
+                    A[j] = temp
+                    
+        return A
+    
---
+++ squares-of-a-sorted-array/main.py
@@ -0,0 +1,4 @@
+from Solution import Solution
+
+solution = Solution()
+print(solution.sortedSquares([-4,-1,0,3,10]))
