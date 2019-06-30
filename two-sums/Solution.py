f29b9a6da869fe03ec3aca246d9c10a98df1f25b
Serdar <serdarsenturk@windowslive.con>
2019-06-30 14:54:54 +0300

    Add two-sums algorithm

---
+++ two-sums/Solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        numbers = []
+        sum = 0
+        
+        
+        for k in range(len(nums)):
+            for m in range(k+1, len(nums)):
+                
+                sum = nums[k] + nums[m]
+                
+                if sum == target:
+                    numbers.append(k)
+                    numbers.append(m)
+    
+                        
+        return numbers
---
+++ two-sums/main.py
@@ -0,0 +1,5 @@
+from Solution import Solution
+
+solution = Solution()
+
+print(solution.twoSum([2,7,11,15], 9))
