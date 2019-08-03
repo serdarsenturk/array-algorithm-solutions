b046513ea38c6eea62ad1cbbc1626792c100b5bd
Serdar <serdarsenturk@windowslive.con>
2019-08-03 14:34:57 +0300

    String Compression
    File name has changed

--- string-compression/main.py
+++ string-compression/main.py
@@ -1,14 +1,14 @@
 def stringCompression(stringInput):
     compString = "" 
-    sayac = 1
+    count = 1
     for i in range(len(stringInput)-1):
         if stringInput[i] == stringInput[i+1]:
-            sayac += 1
+            count += 1
         else:
-            compString += stringInput[i] + str(sayac)
-            sayac = 1
-
-    compString += stringInput[i] + str(sayac)
+            compString += stringInput[i] + str(count)
+            count = 1
+        
+        compString += stringInput[i] + str(count)
 
     if len(compString) >= len(stringInput):
         return stringInput
